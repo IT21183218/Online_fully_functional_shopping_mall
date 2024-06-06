f1859c063c29a40b49920f651a58394f628365cf
IT21183218 <99754850+IT21183218@users.noreply.github.com>
2024-06-07 00:03:41 +0530

    my_function
    my_function

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "./screen/Login";
+import Register from "./screen/Register";
+import ViewProfile from "./screen/ViewProfile";
+import Home from "./screen/Home";
+import EditProfile from "./screen/EditProfile ";
+import SpinAndWin from "./screen/SpinAndWin";
+import Main from "./screen/Main";
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="ViewProfile" component={ViewProfile} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Main" component={Main} />
+
+        <Stack.Screen name="Spinner" component={SpinAndWin} />
+        <Stack.Screen name="EditProfile" component={EditProfile} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
